661191de36560967099ee9d6047af0f69a833710
Lars Wikman <lars@underjord.io>
2024-10-11 16:39:23 +0200

    Fix driver setup, works

--- post-build.sh
+++ post-build.sh
@@ -19,6 +19,19 @@ function slim_down_dri_libs() {
 
 slim_down_dri_libs
 
+echo "binaries: $BINARIES_DIR"
+echo "host: $HOST_DIR"
+echo "staging: $STAGING_DIR"
+echo "target: $TARGET_DIR"
+echo "build: $BUILD_DIR"
+mkdir -p $BINARIES_DIR/uconsole
+cp $BUILD_DIR/linux-custom/arch/arm/boot/dts/overlays/devterm-bt.dtbo $BINARIES_DIR/uconsole/
+cp $BUILD_DIR/linux-custom/arch/arm/boot/dts/overlays/devterm-panel-uc.dtbo $BINARIES_DIR/uconsole/
+cp $BUILD_DIR/linux-custom/arch/arm/boot/dts/overlays/devterm-misc.dtbo $BINARIES_DIR/uconsole/
+cp $BUILD_DIR/linux-custom/arch/arm/boot/dts/overlays/devterm-pmu.dtbo $BINARIES_DIR/uconsole/
+cp $BUILD_DIR/linux-custom/arch/arm/boot/dts/overlays/devterm-panel.dtbo $BINARIES_DIR/uconsole/
+cp $BUILD_DIR/linux-custom/arch/arm/boot/dts/overlays/devterm-wifi.dtbo $BINARIES_DIR/uconsole/
+
 # Create the fwup ops script to handling MicroSD/eMMC operations at runtime
 # NOTE: revert.fw is the previous, more limited version of this. ops.fw is
 #       backwards compatible.
